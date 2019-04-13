95af21e4e8e2cf620a4816a1cf294d1a6638a963
hansolyoon <dhsol1126@gmail.com>
2019-04-13 14:36:52 -0600

    no color

--- playground/src/state.ts
+++ playground/src/state.ts
@@ -150,7 +150,7 @@ export class State {
   collectStats = false;
   numHiddenLayers = 1;
   hiddenLayerControls: any[] = [];
-  networkShape: number[] = [4, 2];
+  networkShape: number[] = [3, 2];
   x = true;
   y = true;
   xTimesY = false;
