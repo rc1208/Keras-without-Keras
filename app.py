4df94ad2c3f96291ec1b81ad36663153e94fbdd7
sishen <shensilasg@gmail.com>
2019-04-21 22:06:15 -0600

    1. add jQuery/AJAX in tabluar_upload_singlefile.html to check dataid new; 2. use SQLite instead of text to save data inventory

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # import the nessecary pieces from Flask
+import sqlite3
+from flask import current_app, g
+from flask.cli import with_appcontext
 from flask import Flask,render_template, request,jsonify,Response, flash, redirect
 import os
 import csv
 import json
 from datetime import datetime
 from werkzeug.utils import secure_filename
+from flask.cli import with_appcontext
 #Create the app object that will route our calls
 app = Flask(__name__)
 # Add a single endpoint that we can use for testing
 
 
-app.config["UPLOAD_DATA_FOLDER"]=app.instance_path + "/data/dataContent/"
-app.config["UPLOAD_INVECTORY_FOLDER"]=app.instance_path + "/data/dataInventory/"
+
+app.config["UPLOAD_DATA_FOLDER"]=app.instance_path + "/data/"
 app.secret_key="123789456"
+app.config["DATABASE"] = app.instance_path + "/data.db"
+app.config["DBTABLE_DATA"] = "training_data"
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 
 @app.route('/', methods = ['GET'])
@@ -37,7 +48,7 @@ def tabular_upload_post():
             os.makedirs(app.config["UPLOAD_DATA_FOLDER"])
         if_ignore_1stline = request.form.get("ignoreHeader")
         if_target_category = request.form.get("categorical")
-        filepath = os.path.join(app.config["UPLOAD_DATA_FOLDER"], "training.txt")
+        filepath = os.path.join(app.config["UPLOAD_DATA_FOLDER"], "training.csv")
         if(request.form.get("dataid")):
             tabular_savefile(file, request.form.get('dataid'), request.form.get('datadesc'),  if_ignore_1stline, if_target_category)
         else:
@@ -65,30 +76,68 @@ def tabular_getsize(filepath, if_target_category, if_ignore_1stline):
     n_output = len(set_labels)
     return (n_feature, n_output)
 
+    
+
+
+
 #
-def tabular_savefile(file, file_id, file_description, if_ignore_1stline, if_target_category):
-    # save the file into fileid, save the inventory, link file to training.txt
-    filepath = os.path.join(app.config["UPLOAD_DATA_FOLDER"], file_id)
+def tabular_savefile(file, data_id, data_desc, if_ignore_1stline, if_target_category):
+    #save the file (name as data_id), and link to "training.csv"
+    filepath = os.path.join(app.config["UPLOAD_DATA_FOLDER"], data_id)
     file.save(filepath)
-    training_file=os.path.join(app.config["UPLOAD_DATA_FOLDER"], "training.txt")
+    training_file=os.path.join(app.config["UPLOAD_DATA_FOLDER"], "training.csv")
     if(os.path.exists(training_file)):
         os.remove(training_file)
     os.symlink(filepath, training_file)
-    if(not os.path.isdir(app.config["UPLOAD_INVECTORY_FOLDER"])):
-        os.makedirs(app.config["UPLOAD_INVECTORY_FOLDER"])
-    invpath = os.path.join(app.config["UPLOAD_INVECTORY_FOLDER"], file_id)
+    #save file info to db
+    conn = get_db()
+    conn.execute('''
+    insert into %s (id, type, description, date_created, file_number, if_ignore_1stline, if_target_category) values (?,?,?,?,?,?,?)''' %app.config["DBTABLE_DATA"], \
+                 (data_id, "tabular", data_desc, datetime.now().strftime("%Y-%m-%d %H-%M-%S"), 1, if_ignore_1stline, if_target_category) )
+    conn.commit()
+''' 
+    invpath = os.path.join(app.config["UPLOAD_INVECTORY_FOLDER"], data_id)
     inv_dict = dict()
-    inv_dict["data_id"] = file_id
+    inv_dict["data_id"] = data_id
     now=datetime.now()
     strdate = now.strftime("%Y-%m-%d %H-%M-%S")
     inv_dict["dated_created"] = strdate
     inv_dict["data_type"] = "tabular"
     inv_dict["file_number"] = 1
-    inv_dict["data_description"] = file_description
+    inv_dict["data_description"] = data_desc
     inv_dict["if_ignore_1stline"] = if_ignore_1stline
     inv_dict["if_target_category"] = if_target_category
     with open(invpath, "w") as f:
         json.dump(inv_dict, f)
+'''
+
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/check_dataid_ok/<dataid>', methods=["POST", "GET"])
+def check_dataid_existence(dataid):
+    db=get_db()
+    sqlcmd = "SELECT * FROM training_data WHERE id = ?"
+    print(sqlcmd)
+    cur=db.execute(sqlcmd, (dataid,))
+    rv = cur.fetchone()
+    if(rv is None):
+        return 'SUCCESS'
+    else:
+        return 'FAIL'
+
+
 
 
 #When run from command line, start the server
