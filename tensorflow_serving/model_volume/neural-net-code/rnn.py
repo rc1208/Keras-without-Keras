1ee9601d87e248d8a66f4e65542ffacbff7acaeb
Ganesh Chandra <ganesh.umesh007@gmail.com>
2019-04-23 19:04:03 -0600

    rnn class added

--- tensorflow_serving/model_volume/neural-net-code/rnn.py
+++ tensorflow_serving/model_volume/neural-net-code/rnn.py
@@ -1,9 +1,40 @@
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, Masking, Embedding
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants, signature_constants
 
 class rnn:
-    def design_model(self,vocab_size,output_d,max_len):
-        model = Sequential()
-        model.add(Embedding(input_dim=vocab_size, output_dim=output_d, input_length=max_len))
-        model.add(Masking(mask_value=0.0))
-        model.add(LSTM(64, return_sequences=False, dropout=0.1, recurrent_dropout=0.1))
+    def __init__(self):
+        self.model = Sequential()
+
+    def design_model(self,vocab_size,output_d,max_len,lstm_out,lstm_drop,lstm_recc_drop,dense_out,reg_dropout):
+        self.model.add(Embedding(input_dim=vocab_size, output_dim=output_d, input_length=max_len))
+        self.model.add(Masking(mask_value=0.0))
+        self.model.add(LSTM(lstm_out, return_sequences=False, dropout=lstm_drop, recurrent_dropout=lstm_recc_drop))
+        self.model.add(Dense(dense_out, activation='relu'))
+        self.model.add(Dropout(reg_dropout))
+        self.model.add(Dense(vocab_size, activation='softmax'))
+
+    def model_comile(self):
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def model_save(self,folder,model_version):
+        sess = tf.Session()
+        x = self.model.input
+        y = self.model.output
+        prediction_signature = tf.saved_model.signature_def_utils.predict_signature_def({"inputs": x},{"prediction": y})
+        valid_prediction_signature = tf.saved_model.signature_def_utils.is_valid_signature(prediction_signature)
+        if (valid_prediction_signature == False):
+            raise ValueError("Error: Prediction signature not valid!")
+        builder = saved_model_builder.SavedModelBuilder(folder + model_version)
+        legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+        builder.add_meta_graph_and_variables(
+            sess, [tag_constants.SERVING],
+            signature_def_map={
+                signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature,
+            },
+            legacy_init_op=legacy_init_op)
+
+        # save model
+        builder.save()
