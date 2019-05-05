fa5c3c33fe87d08c94f603488313871c677d8f47
sishen <shensilasg@gmail.com>
2019-05-05 17:22:42 -0600

    finish data list

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import sqlite3
 from flask import current_app, g
 from flask import Flask,render_template, request,jsonify,Response, flash, redirect
+from flask import send_file, send_from_directory
 import os
 import csv
 import json
@@ -28,6 +29,10 @@ app.secret_key="123789456"
 app.config["DATABASE"] = app.instance_path + "/data.db"
 app.config["DBTABLE_DATA"] = "training_data"
 
+app.config["LOSS_TABULAR_CATEGORY"] = "categorical_crossentropy"
+app.config["LOSS_TABULAR_NUMERIC"] = "mse"
+app.config["LOSS_IMAGES"] = "categorical_crossentropy"
+app.config["LOSS_TEXT"] = "categorical_crossentropy"
 
 @app.route('/', methods = ['GET'])
 def home():
@@ -58,9 +63,9 @@ def tabular_upload_post():
             file.save(filepath)
         size_input_neuron, size_output_neuron = tabular_getsize(filepath, if_target_category, if_ignore_1stline)
         print((if_target_category, if_ignore_1stline, size_input_neuron, size_output_neuron))
-        lossfunc = "mse"
+        lossfunc = app.config["LOSS_TABULAR_NUMERIC"]
         if(if_target_category == "on"):
-            lossfunc ="crossentropy"
+            lossfunc =app.config["LOSS_TABULAR_CATEGORY"]
         return redirect("http://127.0.0.1:8080/#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&regularizationRate=0&noise=0&networkShape=3,2&seed=0.44887&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, lossfunc, filepath))
 
 
@@ -204,7 +209,7 @@ def images_upload_post():
     if 'inputfilePkl' in request.files:
         # upload pickle
         file = request.files['inputfilePkl']
-        edir_pickle=app.config["UPLOAD_DATA_FOLDER"]+"/images/"
+        dir_pickle=app.config["UPLOAD_DATA_FOLDER"]+"/images/"
         if(not os.path.isdir(dir_pickle)):
             os.makedirs(dir_pickle)
         filepath = os.path.join(dir_pickle, "training.pickle")
@@ -217,8 +222,8 @@ def images_upload_post():
         if(isinstance(sizes, tuple)):
             size_input_neuron = sizes[1]*sizes[2]
             size_output_neuron = sizes[3]
-            print("http://127.0.0.1:8080/index_cnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=1&regularizationRate=0&noise=0&networkShape=1,1,4,4&seed=0.35115&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, "crossentropy", filepath))
-            return redirect("http://127.0.0.1:8080/index_cnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=1&regularizationRate=0&noise=0&networkShape=1,1,4,4&seed=0.35115&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, "crossentropy", filepath))
+            print("http://127.0.0.1:8080/index_cnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=1&regularizationRate=0&noise=0&networkShape=1,1,4,4&seed=0.35115&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, app.config["LOSS_IMAGES"], filepath))
+            return redirect("http://127.0.0.1:8080/index_cnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=1&regularizationRate=0&noise=0&networkShape=1,1,4,4&seed=0.35115&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, app.config["LOSS_IMAGES"], filepath))
         else:
             return redirect(request.url)
     elif 'inputfilePNG' in request.files and 'inputfileCsv' in request.files:
@@ -287,7 +292,7 @@ def text_upload_post():
             text_savefile(file, request.form.get('dataid'), request.form.get('datadesc'), dir_text)
         else:
             file.save(filepath)
-        lossfunc="crossentropy"
+        lossfunc=app.config["LOSS_TEXT"]
         return redirect("http://127.0.0.1:8080/index_rnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=2&regularizationRate=0&noise=0&networkShape=1&seed=0.09947&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&lossfunc=%s&dataLocation=%s" %(lossfunc, filepath))
         
 def text_savefile(file, data_id, data_desc, dir_text):
@@ -303,7 +308,90 @@ def text_savefile(file, data_id, data_desc, dir_text):
     insert into %s (id, type, description, date_created, file_number, if_ignore_1stline, if_target_category) values (?,?,?,?,?,?,?)''' %app.config["DBTABLE_DATA"], \
                  (data_id, "text", data_desc, datetime.now().strftime("%Y-%m-%d %H-%M-%S"), 1, 0, 0) )
     conn.commit()
-    
+
+@app.route("/data_list/<typex>", methods=["GET"])
+def show_data_list(typex):
+    datalist=get_data_list(typex)
+    rows=[]
+    for dataitem in datalist:
+        row=dict()
+        row["dataid"] = dataitem["id"]
+        row["datadesc"] = dataitem["description"]
+        row["type"] = dataitem["type"]
+        row["date"] = dataitem["date_created"]
+        if(row["type"] == "tabular"):
+            if(dataitem["if_target_category"] == "on"):
+                row["note"] = "Classification"
+                row["lossfunc"] =  app.config["LOSS_TABULAR_CATEGORY"]
+            else:
+                row["note"] = "Regression"
+                row["lossfunc"] =  app.config["LOSS_TABULAR_NUMERIC"]
+        elif(row["type"] == "images"):
+            row["note"] = "Classification"
+            row["lossfunc"] =  app.config["LOSS_IMAGES"]
+        else:
+            row["note"] = "Language Model"
+            row["lossfunc"] = app.config["LOSS_TEXT"]
+        row["url"] = "/download_data/%s/%s" %(row["type"], row["dataid"])
+        row["data_location"] = get_data_dir(row["type"], row["dataid"]) + "/" + row["dataid"]
+        rows.append(row)
+    return render_template("list_data_template.html", data=rows)
+
+
+def get_data_list(typex):
+    db=get_db()
+    if(typex == "all"):
+        sqlcmd = "SELECT * FROM training_data"
+    else:
+        sqlcmd = "SELECT * FROM training_data where type = '%s'" %typex
+    print(sqlcmd)
+    cur=db.execute(sqlcmd)
+    rv = cur.fetchall()
+    return rv
+
+def get_data_dir(typex, dataid):
+    if(typex == "tabular"):
+        dirx = app.config["UPLOAD_DATA_FOLDER"] + "/"
+    elif(typex == "images"):
+        dirx = app.config["UPLOAD_DATA_FOLDER"] + "/images/"
+    else:
+        dirx = app.config["UPLOAD_DATA_FOLDER"] + "/text/"
+    return dirx
+
+@app.route("/download_data/<typex>/<dataid>", methods = ['GET'])
+def download_file(typex, dataid):
+    print("download_file")
+    dirx = get_data_dir(typex, dataid)
+    print(dataid)
+    return send_from_directory(dirx, dataid, as_attachment=True) 
+
+@app.route("/data_select", methods = ['POST'])
+def use_data_nn():
+    typex = request.form.get("type")
+    data_location = request.form.get("data_location")
+    lossfunc = request.form.get("lossfunc")
+    task = request.form.get("task")
+    if(typex == "tabular"):
+        if(task == "Classification"):
+            size_input_neuron, size_output_neuron = tabular_getsize(data_location, 1, 1)
+        else:
+            size_input_neuron, size_output_neuron = tabular_getsize(data_location, 0, 1)
+        print((size_input_neuron, size_output_neuron))
+        return redirect("http://127.0.0.1:8080/#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&regularizationRate=0&noise=0&networkShape=3,2&seed=0.44887&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, lossfunc, data_location))
+    elif(typex == "images"):
+        sizes=images_getsize(data_location)
+        if(isinstance(sizes, tuple)):
+            size_input_neuron = sizes[1]*sizes[2]
+            size_output_neuron = sizes[3]
+            return redirect("http://127.0.0.1:8080/index_cnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=1&regularizationRate=0&noise=0&networkShape=1,1,4,4&seed=0.35115&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&sizeInput=%d&sizeOutput=%d&lossfunc=%s&dataLocation=%s" %(size_input_neuron, size_output_neuron, app.config["LOSS_IMAGES"], data_location))
+        else:
+            return redirect(request.url)
+    else:
+        return redirect("http://127.0.0.1:8080/index_rnn.html#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&typeofnet=2&regularizationRate=0&noise=0&networkShape=1&seed=0.09947&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&discretize_hide=true&showTestData_hide=true&stepButton_hide=true&noise_hide=true&dataset_hide=true&lossfunc=%s&dataLocation=%s" %(lossfunc, data_location))
+
+        
+
+
 
 #When run from command line, start the server
 if __name__ == '__main__':
