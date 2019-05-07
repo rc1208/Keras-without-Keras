9200c801e36dfe4773a94efee648c3592ce5509f
Chu-Sheng Ku <bamboo983@gmail.com>
2019-05-07 14:48:20 -0600

    Update default RNN configuration

--- playground/src/playground_rnn.ts
+++ playground/src/playground_rnn.ts
@@ -207,15 +207,16 @@ function makeGUI() {
       acts += "sigmoid";
       xhttp.send(
         JSON.stringify({
-          "nn_type": "feedforward",
-          "hidden_list": hidden_list,
-          "inp": String(state.sizeInput),
-          "activation_list": acts,
-          "optimiser":"adam",
+          "nn_type": "rnn",
+          "lstm_out": "256",
+          "dense_out": "100",
+          "reg_dropout": "0.2",
+          "batch_size": "1000",
+          "optimiser": "adam",
           "split_value": "0.2",
           "loss_function": state.lossfunc,
           "data_location": state.dataLocation,
-          "epochs": 10
+          "epochs": "10"
         })
       );
       xhttp.onreadystatechange=(e) => {
