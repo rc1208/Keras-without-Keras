22b404101c69ce1a0a85e2b148d67f558bacbd0e
rc1208 <chowdhuryrahul5@gmail.com>
2019-05-07 20:59:15 -0600

    added test rnn data

--- request.py
+++ request.py
@@ -12,7 +12,7 @@ res = requests.post('http://localhost:3333/api/neural-network/v1.0/', json={"nn_
 if res.ok:
     print(res)
 
-'''
+
 
 res_cnn = requests.post('http://localhost:3333/api/neural-network/v1.0/', json={"nn_type":"cnn", \
   "hidden_list":"64 32 4", \
@@ -26,8 +26,7 @@ res_cnn = requests.post('http://localhost:3333/api/neural-network/v1.0/', json={
   "loss_function": "categorical_crossentropy", \
   "data_location":"data/mnist21x21_3789_converted.pklz"})
 if res_cnn.ok:
-    print("Model Combined!")
-
+    print("Model Compiled")
 
 '''
 
@@ -40,7 +39,6 @@ res_rnn = requests.post('http://localhost:3333/api/neural-network/v1.0/', json={
 "optimiser":"adam",
 "split_value": "0.2",
 "loss_function": "categorical_crossentropy",
-"data_location":"data/asyoulikeit.txt"})
+"data_location":"data/cnn-test.txt"})
 if res_rnn.ok:
-    print("Model Combined!")
-'''
+    print("Model Compiled!")
